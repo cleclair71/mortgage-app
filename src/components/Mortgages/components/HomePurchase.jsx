9ebd4b140b093de7095db885125d4d41a71f510b
cleclair71 <cassieleclair71@gmail.com>
2023-07-06 17:15:41 -0400

    lists

--- src/components/Mortgages/components/HomePurchase.jsx
+++ src/components/Mortgages/components/HomePurchase.jsx
@@ -96,10 +96,10 @@ const HomePurchase = () => {
                             By definition “A person's primary residence, or main residence is the dwelling where they usually live, typically a house or an apartment. ... A primary residence is considered to be a legal residence for the purpose of income tax and/or acquiring a mortgage.”
                             In order for your new home to qualify as your primary residence, you must answer yes to the following questions:
                         </Text>
-                        <ol>
-                            <li>Are you going to be living in this home for a majority of the calendar year?</li>
-                            <li>Are you employed in the same city/town or area as the property? (or within reasonable driving distance)</li>
-                        </ol>
+                        <list styleType="disc" pl={5} mb={3}>
+                            <ListItem>Are you going to be living in this home for a majority of the calendar year?</ListItem>
+                            <ListItem>Are you employed in the same city/town or area as the property? (or within reasonable driving distance)</ListItem>
+                        </list>
                         <SpringButton as={Link} to="/doc-page" variant="outline">
                             Documentations
                         </SpringButton>
@@ -123,7 +123,7 @@ const HomePurchase = () => {
                         </Text>
                     </AccordionPanel>
                 </AccordionItem>
-                  {/* New Section: Cottage */}
+                {/* New Section: Cottage */}
                 <AccordionItem>
                     <AccordionButton _expanded={{ bg: 'green', color: 'white' }}>
                         <Box flex="1" textAlign="left">
@@ -133,45 +133,104 @@ const HomePurchase = () => {
                     </AccordionButton>
                     <AccordionPanel pb={4}>
                         <Text mb={3}>
-                        Exploring the possibility of acquiring a picturesque cottage, a tranquil summer residence, or a conveniently located city condominium as your secondary home? Seize the opportunity to leverage the equity in your current home to finance either the down payment or the entire purchase. Generally, a minimum down payment of 5% is required for secondary homes, though certain property types may warrant higher down payment thresholds.
+                            Exploring the possibility of acquiring a picturesque cottage, a tranquil summer residence, or a conveniently located city condominium as your secondary home? Seize the opportunity to leverage the equity in your current home to finance either the down payment or the entire purchase. Generally, a minimum down payment of 5% is required for secondary homes, though certain property types may warrant higher down payment thresholds.
                         </Text>
                         <Text mb={3}>
-                        Before embarking on your quest for the ideal second property, it is imperative to engage in thorough consultations with trusted experts, including your mortgage agent/broker, financial planner, and realtor. These seasoned advisors will assist you in establishing a well-defined purchase budget, unraveling potential tax implications, and ensuring the identification of a property that impeccably aligns with your specific requirements.
+                            Before embarking on your quest for the ideal second property, it is imperative to engage in thorough consultations with trusted experts, including your mortgage agent/broker, financial planner, and realtor. These seasoned advisors will assist you in establishing a well-defined purchase budget, unraveling potential tax implications, and ensuring the identification of a property that impeccably aligns with your specific requirements.
                         </Text>
                         <Text mb={3}>
-                        When seeking a charming summer cottage, bear in mind that lenders impose certain restrictions on property types, which influence the corresponding down payment criteria. For properties meeting the Type A Criteria (see below), securing a mortgage with a mere 5% down payment is feasible. Conversely, Type B properties (see below) typically necessitate a minimum 10% down payment. Opting to forgo Mortgage Default Insurance may call for down payments ranging from 20% to 50%, contingent upon the property criteria. Rely on our expertise to guide you through the myriad options at your disposal.
+                            When seeking a charming summer cottage, bear in mind that lenders impose certain restrictions on property types, which influence the corresponding down payment criteria. For properties meeting the Type A Criteria (see below), securing a mortgage with a mere 5% down payment is feasible. Conversely, Type B properties (see below) typically necessitate a minimum 10% down payment. Opting to forgo Mortgage Default Insurance may call for down payments ranging from 20% to 50%, contingent upon the property criteria. Rely on our expertise to guide you through the myriad options at your disposal.
                         </Text>
                         <Text mb={3}>
-                        In the case of year-round homes designated as secondary properties, a 5% down payment is generally applicable, affording you the opportunity to fulfill your aspirations.
+                            In the case of year-round homes designated as secondary properties, a 5% down payment is generally applicable, affording you the opportunity to fulfill your aspirations.
                         </Text>
                         <Text mb={3}>
-                        If your country abode leaves you yearning for a weekday sanctuary in the bustling city, fear not. Typically, a down payment as low as 5% suffices to secure a mortgage for such desirable condominium acquisitions.
+                            If your country abode leaves you yearning for a weekday sanctuary in the bustling city, fear not. Typically, a down payment as low as 5% suffices to secure a mortgage for such desirable condominium acquisitions.
                         </Text>
                         <Text mb={3}>
-                        Unveil the perfect vacation home that harmonizes with your fiscal prudence and lifestyle preferences. Entrust us with the privilege of transforming your cherished dream of owning a captivating getaway into an awe-inspiring reality.
+                            Unveil the perfect vacation home that harmonizes with your fiscal prudence and lifestyle preferences. Entrust us with the privilege of transforming your cherished dream of owning a captivating getaway into an awe-inspiring reality.
                         </Text>
                         <SpringButton as={Link} to="/doc-page" variant="outline" mb={3}>
                             Important Documents
                         </SpringButton>
                         <Text mb={3}>
-                        Figuring out your second home budget is another thing to think about. Here is a list we have compiled:
+                            Figuring out your second home budget is another thing to think about. Here is a list we have compiled:
                         </Text>
                         <List styleType="disc" pl={5} mb={3}>
-                                        <ListItem>Insurance</ListItem>
-                                        <ListItem>Property taxes</ListItem>
-                                        <ListItem>Utilities (is it 3 season or 4?)</ListItem>
-                                        <ListItem>Maintenance and repairs</ListItem>
-                                        <ListItem>What will your mortgage be?</ListItem>
-                                        <ListItem> Entertainment (TV, Internet, phone, etc.)</ListItem>
-                                    </List>
+                            <ListItem>Insurance</ListItem>
+                            <ListItem>Property taxes</ListItem>
+                            <ListItem>Utilities (is it 3 season or 4?)</ListItem>
+                            <ListItem>Maintenance and repairs</ListItem>
+                            <ListItem>What will your mortgage be?</ListItem>
+                            <ListItem> Entertainment (TV, Internet, phone, etc.)</ListItem>
+                        </List>
 
-                        <p>
+                        <Text mb={3}>
                             If you are in the market for your first home, your second or tenth home, give us a call today, I’m here to help find solutions that work for your needs, wants and lifestyle.
-                        </p>
+                        </Text>
+                        <Box
+                            flex="1"
+                            position="relative"
+                            maxHeight="90vh"
+                            margin="auto"
+                            minWidth="200px"
+                            overflowY="auto"
+                            ml={[5, 2]}
+                            mt={[10, 5]}
+                            p={5}
+                            bg="gray.100"
+                            borderRadius="md"
+                            boxShadow="md"
+                            display="flex"
+                            flexDirection={["column", "column", "row"]}
+                        >
+                            <Box flex="1" pr={[0, 0, 5]} mb={[5, 5, 0]}>
+                                <Heading size="sm" mb={3}>
+                                    Secondary Homes (Type A)
+                                </Heading>
+                                <List styleType="disc" pl={5} mb={3}>
+                                    <ListItem>Foundation must be permanent and installed beyond the frost line. This includes concrete/concrete block or preserved wood foundations certified by a professional engineer or post/pier foundations on solid bedrock.</ListItem>
+                                    <ListItem>Must be zoned and used as residential, rural or seasonal. We do not accept mixed uses or rental pooling.</ListItem>
+                                    <ListItem>Freehold or condominium title. We do not accept co-ops or ¼ interest ownership.</ListItem>
+                                    <ListItem>At minimum, property must have a kitchen, 3-piece bathroom, bedroom, and common area.</ListItem>
+                                    <ListItem>Remaining economic life must be 25 years.</ListItem>
+                                    <ListItem> Year-round road access on reasonable quality public roads, serviced by the local municipality. (indent)  We also allow privately serviced roads, provided there is a maintenance contract in place.</ListItem>
+                                    <ListItem>Property must be winterized with a permanent heat source. For example, heating can be baseboard, forced air, water radiator, radiant, coal, propane, geothermal heat pumps, or heat pumps.</ListItem>
+                                    <ListItem>Good quality construction with no signs of deferred maintenance.</ListItem>
+                                    <ListItem> Water source: well, municipal serviced, and cistern. Water source must be drinkable. We accept lake or river water, provided the property has its own filtration system. For example, a reverse osmosis system.</ListItem>
+                                    <ListItem> There must be good market appeal in the area with no adverse influences or neighbourhood nuisances.</ListItem>
+
+                                </List>
+                            </Box>
+                            <Box flex="1">
+                                <Heading size="sm" mb={3}>
+                                    Vacation Homes (Type B)
+                                </Heading>
+                                <text mb={3}>
+                                    All Type A property requirements apply to Type B, except for the
+                                    following:
+                                </text>
+                                <List styleType="disc" pl={5} mb={3}>
+                                    <list styleType="disc" pl={5} mb={3}>
+                                        <ListItem>No permanent heat source is required. For example, a wood stove, fireplace, stove or heat blower is acceptable.</ListItem>
+                                        <ListItem>Foundation may be floating. For example, sitting on blocks.</ListItem>
+                                        <ListItem>Water source needn't be drinkable. However, there must be running water in the home.
+                                            Boat access only accepted.
+                                        </ListItem>
+                                        <ListItem>Seasonal road use is acceptable. This means the road does not have to be plowed during the winter.
+                                        </ListItem>
+                                    </list>
+                                </List>
+                            </Box>
+                        </Box>
+
+
+
+
                     </AccordionPanel>
                 </AccordionItem>
             </Accordion>
-        </Box>
+        </Box >
     );
 }
 
