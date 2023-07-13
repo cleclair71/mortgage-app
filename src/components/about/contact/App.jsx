aa7cb0f9f861105b6f8b0cfdeebc5b8141bdb861
cleclair71 <cassieleclair71@gmail.com>
2023-07-13 15:51:04 -0400

    contact page meta tag

--- src/components/about/contact/App.jsx
+++ src/components/about/contact/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import Header from '../../Main/header/Header';
 import BannerContent from './Banner';
 import Contact from './Contact';
@@ -11,6 +12,11 @@ import HeaderImage from '../../../assets/ficus.png';
 const ContactPage = () => {
     return (
         <>
+            <Helmet>
+                <title>Contact Dora Main AMP | Main Mortgage | The Mortgage Professionals</title>
+                <meta name="description" content="Reach out to Dora Main AMP at Main Mortgage, The Mortgage Professionals. We're here to answer your questions and guide you through the mortgage application process." />
+                <meta name="keywords" content="Dora Main, Main Mortgage, Mortgage Professionals, contact, mortgage questions, AMP, mortgage application, mortgage help, mortgage guidance, customer service, home purchase, mortgage rates, refinancing, Canada" />
+            </Helmet>
             <Box bgImage={`url(${HeaderImage})`} bgSize="cover" bgPos="center" m="1rem" h="100vh">
                 <Header />
                 <BannerContent />
