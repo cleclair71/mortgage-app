7fb2dee5734d2f5b57f73ba80c4eef79b5471e38
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-09-16 21:40:46 -0400

    created simple templates for server setup

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
-export const ADD_CLIENT = gql`
-  mutation addClient($email: String!) {
-    addClient(email: $email) {
+export const ADD_USER = gql`
+  mutation addUser($email: String!) {
+    addUser(email: $email) {
       _id
       email
     }
