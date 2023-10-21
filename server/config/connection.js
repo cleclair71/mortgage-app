ae1b19217a24cc28928912b5c94a81681ee22b14
Calvin Trieu <calvin.trieu@hotmail.com>
2023-10-20 23:50:02 -0400

    testing build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,16 +1,25 @@
-import pg from "pg";
+import pg from 'pg';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const { Pool } = pg;
 
-const config = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-};
+let config;
+
+if (process.env.NODE_ENV === 'production') {
+  config = {
+    connectionString: process.env.PG_RENDER_CONNECTION_STRING,
+  };
+} else {
+  config = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+  };
+}
 
 const pool = new Pool(config);
 
