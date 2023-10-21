d7808fef2f12291b43d96985c6e7cd47e65a8d35
Calvin Trieu <calvin.trieu@hotmail.com>
2023-10-21 01:16:44 -0400

    changed cors to frontend_url

--- server/database.sql
+++ server/database.sql
@@ -1,4 +1,9 @@
 CREATE DATABASE mortgage;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE users (user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),  user_email VARCHAR(255) NOT NULL, user_password VARCHAR(255) NOT NULL);
+CREATE TABLE users (
+    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),  
+    user_email VARCHAR(255) NOT NULL, 
+    user_password VARCHAR(255) NOT NULL,
+    date_created TIMESTAMP NOT NULL);
+    
 
--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ import express from 'express'
 import cookieParser from 'cookie-parser';
 import cors from "cors"
 import router from "./routes/index.js"
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json());
-app.use(cors({ origin: true, credentials: true }))
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
 app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
 
